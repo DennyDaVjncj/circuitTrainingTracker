1d12ef79f6a7111509b27f8b0e297efad3d9c8b3
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-18 08:12:17 -0500

    tentative server configuration

---
+++ waitress.js
@@ -0,0 +1,18 @@
+const xprss=require('express');
+const wildAnimal=require('mongoose');
+const PORT=process.env.PORT||5385;
+const app=xprss();
+
+app.use(xprss.urlencoded({extended:true}));
+app.use(xprss.json());
+app.use(xprss.static('public'));
+
+wildAnimal.connect(process.env.MONGODB_URI||'mongodb://localhost/workoutApp',{
+    useNewUrlParser:true,
+    useFindAndModify:false
+});
+app.use(require('not sure what goes here just yet'));
+app.listen(PORT,()=>{
+    console.log(`application live on port ${PORT}!`)
+});
+//configure the entire server, model from mini project
