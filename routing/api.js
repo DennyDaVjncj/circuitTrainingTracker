50fe516654951329a09857e66e6ec0d97c0c5200
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-22 14:21:06 -0500

    object response achieved

--- routing/api.js
+++ routing/api.js
@@ -1,13 +1,12 @@
 const compass=require('express').Router();
-const CircuitTraining=require('../models/Workout');
+const CircuitTraining=require('../models/workout');
 
 compass.post('/api/exercise',({body},echo)=>{
-    CircuitTraining.create(body).then(endorphins=>{
+    console.log({body});
+    CircuitTraining.create(JSON.parse(body)).then(endorphins=>{
         echo.json(endorphins)
     }).catch(injury=>{
         echo.status(400).json(injury);
     })
 });
-
-
     module.exports=compass;
