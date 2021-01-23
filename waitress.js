c0b9292b8f74b801b7113eb02fbd7a135bb364f7
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-23 07:05:48 -0500

    updated waitress

--- waitress.js
+++ waitress.js
@@ -2,17 +2,20 @@ const xprss=require('express');
 const wildAnimal=require('mongoose');
 const PORT=process.env.PORT||5385;
 const app=xprss();
+const JP=require('morgan');
 
+app.use(JP('dev'));
 app.use(xprss.urlencoded({extended:true}));
 app.use(xprss.json());
 app.use(xprss.static('public'));
 
-wildAnimal.connect(process.env.MONGODB_URI||'mongodb://localhost/workout',{
+wildAnimal.connect(process.env.MONGODB_URI||'mongodb://localhost/trainingRegimen',{
     useNewUrlParser:true,
     useFindAndModify:false,
     useUnifiedTopology:true
 });
 app.use(require('./routing/api'));
+app.use(require('./routing/display'));
 app.listen(PORT,()=>{
     console.log(`application live on port ${PORT}!`)
 });
